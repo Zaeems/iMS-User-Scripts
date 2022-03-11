7bc44d12b79cad4a4d24edddec6615566c3f5634
Zaeem Patel <46079004+Zaeems@users.noreply.github.com>
2022-03-11 11:25:42 +0000

    Update iMS - Light Mode Text Color Fix.user.js

--- iMS - Light Mode Text Color Fix.user.js
+++ iMS - Light Mode Text Color Fix.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         iMS - Light Mode Text Color Fix
 // @namespace    http://imssystems.tech/
-// @version      0.1.1
+// @version      0.1.2
 // @description  Gives all large textboxes a draggable resize button.
 // @author       Zaeem
 // @match        https://*.imssystems.tech/admin/*
@@ -19,6 +19,10 @@ function addGlobalStyle(css) {
     head.appendChild(style);
 }
 
-addGlobalStyle('.text-success {color: #0ab71f!important;}')
+addGlobalStyle('.text-success {color: #008000!important;}')
+addGlobalStyle('.text-danger {color: #ff0000!important;}')
 addGlobalStyle('.card .card-header .card-category {color: #000000;}')
 addGlobalStyle('.white-content .card:not(.card-white) .card-body .card-category, .white-content .card:not(.card-white) .card-body .card-description, .white-content .card:not(.card-white) .card-body .card-title, .white-content .card:not(.card-white) .card-header .card-title {color: #000000;}')
+addGlobalStyle('.text-muted {color: #000000!important;}')
+addGlobalStyle('::-webkit-scrollbar-thumb {background-color: #ff00006b;}')
+addGlobalStyle('::-webkit-scrollbar-track {background-color: #f7d6d666;}')
