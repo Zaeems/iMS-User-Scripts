dcd21d391603d9c39551b3536c85290eb0039ada
Zaeem Patel <46079004+Zaeems@users.noreply.github.com>
2022-03-15 13:21:58 +0000

    Update Remove-Input-Caret.user.js

--- Remove-Input-Caret.user.js
+++ Remove-Input-Caret.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         iMS - Remove Input Caret
 // @namespace    http://imssystems.tech/
-// @version      0.1.1
+// @version      0.1.2
 // @description  Removes input caret from all input boxes.
 // @author       Zaeem
-// @match        https://*.imssystems.tech/admin/*
+// @match        https://*.imssystems.tech/*
 // @icon         https://staging.imssystems.tech/favicon.png
 // ==/UserScript==
 
