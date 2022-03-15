a6556d0db2630ed2b5d3452863ece5689a661db2
Zaeem Patel <46079004+Zaeems@users.noreply.github.com>
2022-03-15 10:20:27 +0000

    Update iMS - Tab Color.user.js

--- iMS - Tab Color.user.js
+++ iMS - Tab Color.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         iMS - Tab Color
 // @namespace    http://imssystems.tech/
-// @version      0.0.2
+// @version      0.0.3
 // @description  Changes tab color to sidebar
 // @author       Zaeem
 // @match        https://*.imssystems.tech/admin/*
