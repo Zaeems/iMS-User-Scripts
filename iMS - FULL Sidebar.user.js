7f7d19f086e86efedc37fe3955f8ab404c77de94
Zaeem Patel <46079004+Zaeems@users.noreply.github.com>
2022-03-16 13:28:15 +0000

    Add files via upload

---
+++ iMS - FULL Sidebar.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         iMS - FULL Sidebar
+// @namespace    http://imssystems.tech/
+// @version      0.1.1
+// @description  Makes the sidebar height double
+// @author       You
+// @match        https://*.imssystems.tech/*
+// @icon         https://staging.imssystems.tech/favicon.png
+// ==/UserScript==
+
+function addGlobalStyle(css) {
+    var head, style;
+    head = document.getElementsByTagName('head')[0];
+    if (!head) { return; }
+    style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = css;
+    head.appendChild(style);
+}
+
+addGlobalStyle('.sidebar {height: 200%!important;}')
+
+();
