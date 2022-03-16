835c08f6d94e013ee7be365c49d483ddc832b305
Zaeem Patel <46079004+Zaeems@users.noreply.github.com>
2022-03-16 13:34:40 +0000

    Update iMS - FULL Sidebar.user.js

--- iMS - FULL Sidebar.user.js
+++ iMS - FULL Sidebar.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         iMS - FULL Sidebar
 // @namespace    http://imssystems.tech/
-// @version      0.1.2
+// @version      0.1.3
 // @description  Makes the sidebar height double
 // @author       You
 // @match        https://*.imssystems.tech/*
@@ -19,5 +19,5 @@ function addGlobalStyle(css) {
 }
 
 addGlobalStyle('.sidebar {height: 200%!important;}')
-addGlobalStyle('.nav-item:first-child a {padding-left: 10rem;}')
+addGlobalStyle('.footer .nav-item:first-child a {padding-left: 10rem;}')
 ();
