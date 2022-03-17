124186fcd61891efaca98b27f3d9d63fa7c79826
Zaeem Patel <46079004+Zaeems@users.noreply.github.com>
2022-03-17 10:33:16 +0000

    Update iMS - FULL Sidebar.user.js

--- iMS - FULL Sidebar.user.js
+++ iMS - FULL Sidebar.user.js
@@ -1,16 +1,17 @@
 // ==UserScript==
+/* globals jQuery, $, waitForKeyElements */
 // @name         iMS - FULL Sidebar
 // @namespace    http://imssystems.tech/
-// @version      0.1.5
+// @version      0.1.8
 // @description  Makes the sidebar height double
 // @author       You
 // @match        https://*.imssystems.tech/*
 // @icon         https://staging.imssystems.tech/favicon.png
 // ==/UserScript==
 
-function toggleOnLoad(){
+/*function toggleOnLoad(){
     document.getElementsByClass("minimize-sidebar")[0].click();
-}
+}*/
 
 function addGlobalStyle(css) {
     var head, style;
@@ -21,9 +22,24 @@ function addGlobalStyle(css) {
     style.innerHTML = css;
     head.appendChild(style);
 }
+/*function toggleOnLoad(){
+  $(document).ready(function() {
+    var doc = $(document).find("switch-1");
+    doc.each(function() {
+       if ($(this).class() == "custom-control-input") {
+         $(this).click();
+       }
+    });
+  });
+}*/
+
+function toggleOnLoad(){
+document.getElementById('switch-1').click(); // = 'true';
+}
 
 addGlobalStyle('.sidebar {height: 200%!important; margin-top: 0px;}')
 addGlobalStyle('.footer .nav {margin-left: 15rem;}')
 addGlobalStyle('.navbar .navbar-brand {margin-left: 11rem;}')
 toggleOnLoad()
+
 ();
