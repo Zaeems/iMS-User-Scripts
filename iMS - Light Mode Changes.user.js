50fe12fb521a1ccbb414445ef1b9d8032fa6fbdc
Zaeem Patel <46079004+Zaeems@users.noreply.github.com>
2022-03-17 11:00:21 +0000

    Update iMS - Light Mode Changes.user.js

--- iMS - Light Mode Changes.user.js
+++ iMS - Light Mode Changes.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         iMS - Light Mode Changes
 // @namespace    http://imssystems.tech/
-// @version      0.1.7
+// @version      0.1.8
 // @description  Makes greens and reds darker.
 // @author       Zaeem
 // @match        https://*.imssystems.tech/*
@@ -18,14 +18,51 @@ function addGlobalStyle(css) {
     head.appendChild(style);
 }
 
-addGlobalStyle('.text-success {color: #008000!important;}')
-addGlobalStyle('.text-danger {color: #ff0000!important;}')
-addGlobalStyle('.card .card-header .card-category {color: #000000;}')
-addGlobalStyle('.white-content .card:not(.card-white) .card-body .card-category, .white-content .card:not(.card-white) .card-body .card-description, .white-content .card:not(.card-white) .card-body .card-title, .white-content .card:not(.card-white) .card-header .card-title {color: #000000;}')
-addGlobalStyle('.text-muted {color: #000000!important;}')
-addGlobalStyle('::-webkit-scrollbar-thumb {background-color: #ff00006b;}')
-addGlobalStyle('::-webkit-scrollbar-track {background-color: #f7d6d666;}')
-addGlobalStyle('.white-content .modal .modal-dialog .modal-content .modal-body h1, .white-content .modal .modal-dialog .modal-content .modal-body h2, .white-content .modal .modal-dialog .modal-content .modal-body h3, .white-content .modal .modal-dialog .modal-content .modal-body h4, .white-content .modal .modal-dialog .modal-content .modal-body h5, .white-content .modal .modal-dialog .modal-content .modal-body h6, .white-content .modal .modal-dialog .modal-content .modal-body p {color: #000000;}')
-addGlobalStyle('.white-content .ReactTable .rt-td, .white-content .ReactTable .rt-thead .rt-th{color: rgb(0 0 0)!important;}')
-addGlobalStyle('.ReactTable .rt-td {color: hsl(0deg 0% 25% / 80%)!important;}')
-addGlobalStyle('.sidebar {background: linear-gradient(0deg,#e14eca,#e14eca);}')
+addGlobalStyle('
+               .text-success {
+               color: #008000!important;
+               }')
+addGlobalStyle('
+               .text-danger {
+               color: #ff0000!important;
+               }')
+addGlobalStyle('
+               .card .card-header .card-category {
+               color: #000000;
+               }')
+addGlobalStyle('
+               .white-content .card:not(.card-white) .card-body .card-category, .white-content .card:not(.card-white) .card-body .card-description, .white-content .card:not(.card-white) .card-body .card-title, .white-content .card:not(.card-white) .card-header .card-title {
+               color: #000000;
+                }')
+addGlobalStyle('
+               .text-muted {
+               color: #000000!important;
+               }')
+addGlobalStyle('
+               ::-webkit-scrollbar-thumb {
+               background-color: #ff00006b;
+               }')
+addGlobalStyle('
+               ::-webkit-scrollbar-track {
+               background-color: #f7d6d666;
+               }')
+addGlobalStyle('
+               .white-content .modal .modal-dialog .modal-content .modal-body h1, .white-content .modal .modal-dialog .modal-content .modal-body h2, .white-content .modal .modal-dialog .modal-content .modal-body h3, .white-content .modal .modal-dialog .modal-content .modal-body h4, .white-content .modal .modal-dialog .modal-content .modal-body h5, .white-content .modal .modal-dialog .modal-content .modal-body h6, .white-content .modal .modal-dialog .modal-content .modal-body p {
+               color: #000000;
+               }')
+addGlobalStyle('
+               .white-content .ReactTable .rt-td, .white-content .ReactTable .rt-thead .rt-th {
+                color: rgb(0 0 0)!important;
+                }')
+addGlobalStyle('
+               .ReactTable .rt-td {
+                color: hsl(0deg 0% 25% / 80%)!important;
+               }')
+addGlobalStyle('
+               .sidebar {
+                background: linear-gradient(0deg,#e14eca,#e14eca);
+                }')
+addGlobalStyle('
+               body {
+                font-family: "Trebuchet MS"
+               }')
