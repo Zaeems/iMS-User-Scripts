df4d81c126e8d07559a60401af0ced5ead9568cd
Zaeem Patel <46079004+Zaeems@users.noreply.github.com>
2022-05-31 09:25:22 +0100

    Update Admin Center (Dark).user.js

--- Scripts/Admin Center (Dark).user.js
+++ Scripts/Admin Center (Dark).user.js
@@ -2,10 +2,10 @@
 /* globals jQuery, $, waitForKeyElements */
 // @name         Admin Center (Dark)
 // @namespace    http://imssystems.tech/
-// @version      0.2.2
+// @version      0.2.3
 // @description  try to take over the world!
 // @author       You
-// @match      https://*.imssystems.tech/admin/*
+// @match        https://*.imssystems.tech/*
 // @icon         https://staging.imssystems.tech/favicon.png
 // @grant GM_log
 // @grant window.onurlchange
@@ -55,7 +55,7 @@ var notificationText = '#FFFFFF' // notification text color
 
 // -- END OF SETTINGS --
 
-// variable declarations
+ // variable declarations
 document.documentElement.style.setProperty('--sidebar-colour', sidebarColor);
 document.documentElement.style.setProperty('--top-colour', topColor);
 document.documentElement.style.setProperty('--tab-colour', tabColor);
@@ -71,30 +71,38 @@ document.documentElement.style.setProperty('--progress-color', progressColor);
 document.documentElement.style.setProperty('--notification-bg-color', notificationBackground);
 document.documentElement.style.setProperty('--notification-fg-color', notificationText);
 
+ // -- LATEST FEATURES --
 
-    // new changes
+ // new changes
+addGlobalStyle('div>i.tim-icons, div>i.fas{background-color: var(--card-bg-color);}') // change icon background color
+addGlobalStyle('div.info-icon { background-image:none !important; background-color: var(--card-bg-color) !important; }') // change icon background color
+addGlobalStyle('.notification-menu {background-color: var(--notification-bg-color);') // chang notification menu backgorund color
+addGlobalStyle('.text-default {color: var(--notification-fg-color) !important;') // change notification font color
+addGlobalStyle('.react-select__menu {background-color: var(--notification-bg-color) !important;') // change dropdown menu background color
 //addGlobalStyle('span, div, a, th, td, p, h1, h2, h3, h4, h5, h6, input {font-family: Verdana !important;}') // change fonts to verdana
 //addGlobalStyle('.sidebar .nav li>a{text-transform:none;}') // remove uppercase sidebar
 //addGlobalStyle('b.caret {display: none;}') // hide sidebar caret
 //addGlobalStyle('.sidebar .nav>li>a>p{font-size: var(--sidebar-header-font-size);}') // make header larger
 //addGlobalStyle('.sidebar .nav li>a{font-size: var(--sidebar-subheader-font-size);}') // make subheader larger
+
+ // previous
 addGlobalStyle('.card{background-color: var(--card-bg-color);}') // set card bg color
 addGlobalStyle('.block {display:none;}') // hide block display on users profile page
-addGlobalStyle('.progress-bar{background-color: var(--progress-color);}')
 addGlobalStyle('.modal-content {background-color:var(--card-bg-color) !important;}') // set modal bg color to card bg color
-addGlobalStyle('div>i.tim-icons, div>i.fas{background-color: var(--card-bg-color);}')
-addGlobalStyle('div.info-icon { background-image:none !important; background-color: var(--card-bg-color) !important; }')
-addGlobalStyle('.notification-menu {background-color: var(--notification-bg-color);')
-addGlobalStyle('.text-default {color: var(--notification-fg-color) !important;')
 
-    // previous
+
+ // -- Older features --
+
+addGlobalStyle('.progress-bar{background-color: var(--progress-color);}') // progress bar color -> variable
 addGlobalStyle('.google-map {height:80vh;}') // resize google map (width is automatic)
-    // modal (and respectively sidebar) resize
+
+ // modal (and respectively sidebar) resize
 addGlobalStyle('.modal-content {height:var(--modal-height;)}') // set modal height
 addGlobalStyle('.modal-content {width:var(--modal-width);}') // set modal width
 addGlobalStyle('.sidebar {width: var(--sidebar-width);}') // set sidebar width to variable value
 addGlobalStyle('.modal-dialog {margin-left: calc(var(--sidebar-width) + 10px); margin-top: 1rem;}') // make modal start next to sidebar
-    // make green and red texts darker, to be more readable
+
+ // make green and red texts darker, to be more readable
 addGlobalStyle('.text-success {color: #008000!important;}')
 addGlobalStyle('.text-danger {color: #ff0000!important;}')
 addGlobalStyle('.btn-success.btn-link {color: #00b390;}')
@@ -102,35 +110,41 @@ addGlobalStyle('.btn-info.btn-link {color: #1d8cf8 !important;}')
 addGlobalStyle('.btn-warning.btn-link {color: #ff8d72 !important;}')
 addGlobalStyle('.btn-danger.btn-link {color: #ff0057 !important;}')
 addGlobalStyle('.ReactTable .rt-thead .rt-th.-cursor-pointer>div:first-of-type:after, .ReactTable .rt-thead .rt-th.-cursor-pointer>div:first-of-type:before {opacity: 0.7;}') // adjust sortBy to be more visible
-    // make main scrollbar bg color same as body
+
+ // make main scrollbar bg color same as body
 addGlobalStyle('::-webkit-scrollbar-track {background-color: var(--content-bg-color);}')
-    // adjust sidebar color
+
+ // adjust sidebar color
 addGlobalStyle('.sidebar {background: var(--sidebar-colour);}')
 addGlobalStyle('.off-canvas-sidebar[data=blue], .sidebar[data=blue] {background: var(--sidebar-colour);}') // blue -> variable
 addGlobalStyle('.off-canvas-sidebar[data=green], .sidebar[data=green] {background: var(--sidebar-colour);}') // green -> variable
 addGlobalStyle('.off-canvas-sidebar[data=orange], .sidebar[data=orange] {background: var(--sidebar-colour);}') // orange -> variable
 addGlobalStyle('.off-canvas-sidebar[data=red], .sidebar[data=red] {background: var(--sidebar-colour);}') // red -> variable
 addGlobalStyle('.off-canvas-sidebar[data=primary], .sidebar[data=primary] {background: var(--sidebar-colour);}') // purple -> variable
-    // adjust top border color
+
+ // adjust top border color
 addGlobalStyle('.main-panel[data=blue]{border-top: var(--top-colour);}') // blue -> variable
 addGlobalStyle('.main-panel[data=green]{border-top: var(--top-colour);}') // green -> variable
 addGlobalStyle('.main-panel[data=orange]{border-top: var(--top-colour);}') // orange -> variable
 addGlobalStyle('.main-panel[data=red]{border-top: var(--top-colour);}') // red -> variable
 addGlobalStyle('.main-panel[data=primary]{border-top: var(--top-colour);}') // purple -> variable
-    // FULLSIDEBAR
+
+ // FULLSIDEBAR
 addGlobalStyle('.sidebar {height: 200%!important; margin-top: 0px;border-radius: 0px;}') // Improved sidebar
 addGlobalStyle('.footer .nav {margin-left: 15rem;}') // Shove footer to side to accomodate sidebar resize
 addGlobalStyle('.navbar .navbar-brand {margin-left: 11rem;}') // Shove navbar to side to accomodate sidebar resize
 addGlobalStyle('.sidebar {margin-left:0px;}') // Start sidebar at left of screen
-    // REMOVE Toggle button
-addGlobalStyle('.navbar-minimize-fixed { display:none; }')
-    // RESIZABLETEXTBOXES
+addGlobalStyle('.navbar-minimize-fixed { display:none; }') // remove toggle button
+
+ // RESIZABLETEXTBOXES
 addGlobalStyle('textarea.form-control {resize: vertical;}')
 addGlobalStyle('input { caret-color: transparent; }') // remove input caret
-    // TABCOLOR
+
+ // TABCOLOR
 addGlobalStyle('.nav-pills.nav-pills-info .nav-item .nav-link.active, .nav-pills.nav-pills-info .nav-item .nav-link.active:focus, .nav-pills.nav-pills-info .nav-item .nav-link.active:hover{background-color: var(--tab-colour);}')
 addGlobalStyle('.nav-pills.nav-pills-info .nav-item .nav-link.active, .nav-pills.nav-pills-info .nav-item .nav-link.active:focus, .nav-pills.nav-pills-info .nav-item .nav-link.active:hover{background-image: var(--tab-colour);}')
 addGlobalStyle('.nav-pills .nav-item .nav-link {background-color: var(--inactive-tab-color);}')
-    // Fix body + footer going blue
+
+ // Fix body + footer going blue
 addGlobalStyle('.main-panel>.content {background-color: var(--content-bg-color);}')
 addGlobalStyle('.footer {background-color: var(--content-bg-color);}')
